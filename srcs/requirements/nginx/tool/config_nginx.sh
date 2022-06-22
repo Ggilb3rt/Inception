16e4c71fb2177fe6a9a73810e6193e2fe8a7e532
ggilbert <ggilbert@student.42.fr>
2022-06-22 21:00:53 +0200

    all work

--- srcs/requirements/nginx/tool/config_nginx.sh
+++ srcs/requirements/nginx/tool/config_nginx.sh
@@ -16,25 +16,7 @@ printf "server {
 			try_files \$uri \$uri/ /index.php?\$args;
 		}
 		
-		location ~ \.php$ {
-			#fastcgi_split_path_info ^(.+?\.php)(/.*)$;
-			#if (!-f \$document_root\$fastcgi_script_name) {
-    		#    return 404;
-    		#}
-			# Mitigate https://httpoxy.org/ vulnerabilities
-    		#fastcgi_param HTTP_PROXY "";
-
-		    #fastcgi_pass ${PHP_HOST}:${PHP_PORT};
-		    #fastcgi_index index.php;
-			#fastcgi_intercept_errors on;
-		
-		    # include the fastcgi_param setting
-			#include fastcgi_params;
-			
-			# include snippets/fastcgi-php.conf;
-			# fastcgi_pass unix:/run/php/php7.3-fpm.sock;
-		
-		
+		location ~ \.php$ {	
 			try_files \$uri =404;
             fastcgi_split_path_info ^(.+\.php)(/.+)$;
             fastcgi_pass ${PHP_HOST}:${PHP_PORT};
@@ -42,22 +24,11 @@ printf "server {
             include fastcgi_params;
             fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
             fastcgi_param PATH_INFO \$fastcgi_path_info;
-		
-		
-		
-		
 		}
-
-		#location ~* \.(js|css|png|jpg|jpeg|gif|ico)$ {
-		#	expires max;
-		#	log_not_found off;
-		#}
 }
 " > /etc/nginx/sites-available/default
 
 sed -i 's|ssl_protocols TLSv1 TLSv1.1 TLSv1.2;|ssl_protocols TLSv1.2 TLSv1.3;|g' /etc/nginx/nginx.conf
-# cat /etc/nginx/sites-available/default
-
 
 mkdir -p $WP_PATH
 chown -R www-data $WP_PATH
