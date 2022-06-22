06601cde6f4d920c63767a4a5259cacff8556453
ggilbert <ggilbert@student.42.fr>
2022-06-22 20:35:47 +0200

    clean mariadb works

--- srcs/requirements/mariadb/tool/config_mariadb.sh
+++ srcs/requirements/mariadb/tool/config_mariadb.sh
@@ -17,12 +17,6 @@ then
 else
     mysql_install_db
     mysqld --init-file="/tmp/init.sql"
-
-    # mysql -e "CREATE DATABASE ${MYSQL_DATABASE};"
-    # mysql -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-    # mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY 'MYSQL_PASSWORD';"
-    # mysql -e "FLUSH PRIVILEGES;"
-    # mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 fi
 
 mysqld
